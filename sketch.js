be446473f3e8d25caef61141c2acd4fc2bee9973
Jfontanez <jfontanez@192.168.1.5>
2018-03-04 15:28:42 -0500

    Taking a tutoring

--- sketch.js
+++ sketch.js
@@ -2,11 +2,6 @@ function setup() {
 createCanvas(640, 480);
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function setup () {
+  line (15, 25, 70, 90);
 }
