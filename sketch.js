1a5e6d3643f3d5f8a3cfdaaecba12db0aaaf965e
Vincent <vincebusta@gmail.com>
2018-03-06 11:54:22 -0500

    edit js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const CANVAS_WIDTH = 470
+const CANVAS_HEIGHT = 370
+
+function setup() {
+createCanvas(CANVAS_WIDTH,CANVAS_HEIGHT);
+background(225);
+
+}
+
+function draw() {
+
+
+}
+
+let h2 = document.createTextNode('Vincent Bustamante');
+let h1 = document.createTextNide('Bear');
+
+h1.appendChild(body);
+h2.appendChild(body);
