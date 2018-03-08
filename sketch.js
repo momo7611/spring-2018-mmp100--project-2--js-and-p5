e90279cc7e41e054aae235dbee0d68853f7f7218
ChelseyAnn <35973078+ChelseyAnn@users.noreply.github.com>
2018-03-08 13:27:02 -0500

    Ellipse

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const CANVAS_WIDTH = 670;
+const CANVAS_HEIGHT = 570;
+
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    background(200);
+}
+
+function draw() {
+ellipse (50, 500, 80, 80)
+}
