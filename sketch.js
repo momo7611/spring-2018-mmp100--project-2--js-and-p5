4eae848a785bacfb705ec20b3a3c9a7930d73386
Dartistanyc <diego.gutierrez@f705-07-39310.bmcc.cuny.edu>
2018-03-08 13:21:37 -0500

    working on project

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
 function setup() {
-createCanvas(640, 480);
+  createCanvas(470, 370);
+  background (200);
+  ellipseMode (RADIUS)// Set ellipseMode to RADIUS
+  fill(255); // Set fill to white
+  ELLIPSE (50, 50, 30, 30);// Draw white ellipse using RADIUS mode
+  createPoint ();
+  point(0.215, 0.681);
+  point(0.431, 0.250);
+  point(0.792, 0.160);
+  point(1.125, 0.278);
+  point(1.313, 0.465);
+  point(1.653, 0.674);
+  point(1.938, 0.861);
+  point(1.410, 0.903);
+  point(1.167, 0.903);
+  point(1.000, 0.972);
+  point(0.813, 1.035);
+  point(0.722, 1.486);
+  point(0.743, 2.014);
+  point(0.896, 2.542);
+  point(0.021, 2.566);
+  point(0.160, 0.736);
+
+  endPoint ();
 }
 
-function setup () {
-  line (15, 25, 70, 90);
+function draw () {
+
+
 }
