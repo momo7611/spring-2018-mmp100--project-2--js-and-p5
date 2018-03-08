513211e6d1e06f3dc9659a8859fbb4976685247a
Peter Fenoaltea <peter.fenoaltea@f705-05-39315.bmcc.cuny.edu>
2018-03-08 14:40:20 -0500

    more edits

--- sketch.js
+++ sketch.js
@@ -4,28 +4,23 @@ const CANVAS_HEIGHT = 370;
 function setup() {
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     background(200);
-    background('#FA8072')
+    background('grey')
 }
 
 function draw() {
   angleMode(DEGREES);
-    line(111,269,133,201)
 
-    line(133,201,160,161)
-
-    line(111,269,90,166)
-
-    line(90,166,146,94)
-
+    line(90,165,147,94)
+    stroke('pink')
     fill('#FA8072')
 
     arc(195,102,100,190,185,30);
 
-    line(243,125,271,364)
+    //line(243,125,271,364)
 
     line(159,161,220,145)
 
-    line(222,145,215,368)
+    //line(222,145,215,368)
 
     fill('white')
     ellipse(196,87,20,20);
@@ -33,8 +28,19 @@ function draw() {
     fill('black')
     ellipse(196,87,10,10);
 
-    triangle(112,267,130,206,91,162)
+    triangle(110,275,139,200,95,160)
+
+    fill('pink')
+    beginShape();
+    vertex(220,148)
+    vertex(210,366)
+    vertex(266,368)
+    vertex(270,140)
+    endShape();
 
     fill('white')
-    rect(110,120,55,75)
+    angleMode(DEGREES);
+    rotate(35);
+    rect(170,20,58,65)
+
   }
