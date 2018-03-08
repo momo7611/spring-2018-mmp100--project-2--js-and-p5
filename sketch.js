c401da973d84217b36d6e220e56ffa792ad218b0
biancabee <31446723+biancabee@users.noreply.github.com>
2018-03-08 14:48:07 -0500

    initial commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 700;
+
+function setup() {
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  background (700);
+
+}
+
+function draw() {
+  ellipse(50, 50, 80, 80);
+}
