729a6a5149870ee0af0711957042453bcd738c97
MoritakaAwa <rimimori@gmail.com>
2018-03-09 01:34:29 -0500

    Figuring out

--- sketch.js
+++ sketch.js
@@ -1,23 +1,13 @@
-window.onload = function(){
-  let body = document.quertSelector("body"),
-  h1 = document.createElement('h1'),
-  x = document.createTextNobe('Abyssinian'),
-  h2 = document.createElement('h2'),
-  y = document.createTextNobe('Moritaka Awa');
+const CANVAS_WIDTH = 470;
+const CANVAS_HEIGHT = 370;
 
-  body.appendChild(h1);
-  h1.appendChild(x);
-  body.appendChild(h2);
-  h2.appendChild(y);
-};
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    background(200);
+}
 
-
- const CANVAS_WIDTH = 500;
- const CANVAS_HEIGHT = 500;
-
- function setup()  {
-     createCanvas(CANVAS_WIDTH,CANVAS_HEIGHT);
-     background(250);
- };
-
- function draw() {
+function draw() {
+    point(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    line(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    line(0, CANVAS_HEIGHT, CANVAS_WIDTH, 0);
+}
