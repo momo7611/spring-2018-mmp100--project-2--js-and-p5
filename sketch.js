3c1ca31c0662a6ec48b06038d3746e965dd075ab
Jfontanez <jfontanez@192.168.1.3>
2018-03-09 12:18:41 -0500

    I hope this is ok professor I cannot see the loading page, its a blank

--- sketch.js
+++ sketch.js
@@ -1,7 +1,77 @@
-function setup() {
-createCanvas(640, 480);
+<<<<<<< HEAD
+window.oload = function(){
+  let body = document.querySelector("body"),
+  h1 = document.createElement("h1"),
+  x = document,createElement ('Goose');
+  h2 = document.createElement ('h2'),
+  y = document.createTexNode ('Diego F. Futierrez');
+
+  body.appendChild(h1);
+  h1.appendChild(x);
+  body.appendChild(h2);
+  h2.appendChild(y);
 }
 
-function setup () {
-  line (15, 25, 70, 90);
+const CANVAS_WIDTH = 470;
+const CANVAS_HEIGHT = 370;
+
+function setup() {
+  createCanvas(CANVAS_WIDTH ,CANVAS_HEIGHT);
+  background (200);
+};
+
+function draw () {
+
+  fill('green')
+  trianle (2.097, 0.770, 3.090, 1.417, 1.930, 1.451);
+
+    line(0.160, 3.000, 0.360, 1.333)
+    line(0.360, 1.333, 0.826, 0.444)
+    line(0.826, 0.479, 1.410, 0.333)
+    line(1.410, 0.333, 1.764, 0.455)
+    line(1.806, 0.465, 2.368, 0.950)
+    line(2.368, 0.950, 0.360, 1.333)
+    line(3.104, 1.882, 1.764, 0.455)
+    line(1.764, 0.455, 1.430, 1.610)
+    line(1.430, 1.610, 1.235, 2.424)
+    line(1.235, 2.424, 0.360, 1.333)
+    line(0.160, 3.000, 1.340, 3.354)
+    line(1.340, 3.354, 1.528, 4.000)
+    line(1.639, 5.000, 0.160, 3.000)
+
+    fill('red');
+    ellipse(1.430, 0.736, 1.681, 0.780)
+
+=======
+function setup() {
+  createCanvas(470, 370);
+  background (200);
+  ellipseMode (RADIUS)// Set ellipseMode to RADIUS
+  fill(255); // Set fill to white
+  ELLIPSE (50, 50, 30, 30);// Draw white ellipse using RADIUS mode
+  createPoint ();
+  point(0.215, 0.681);
+  point(0.431, 0.250);
+  point(0.792, 0.160);
+  point(1.125, 0.278);
+  point(1.313, 0.465);
+  point(1.653, 0.674);
+  point(1.938, 0.861);
+  point(1.410, 0.903);
+  point(1.167, 0.903);
+  point(1.000, 0.972);
+  point(0.813, 1.035);
+  point(0.722, 1.486);
+  point(0.743, 2.014);
+  point(0.896, 2.542);
+  point(0.021, 2.566);
+  point(0.160, 0.736);
+
+  endPoint ();
+}
+
+function draw () {
+
+>>>>>>> 4eae848a785bacfb705ec20b3a3c9a7930d73386
+
 }
